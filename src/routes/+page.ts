e23c221a7a95f82ed64a45cd81e751091e1c80a5
PedroTurik <pedroturik@gmail.com>
2024-02-21 08:12:22 -0300

    small changes

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,8 @@
 import type { PageLoad } from './$types';
 
 export const load = (async ({ fetch }) => {
-    let roomsAmount = await (await fetch('http://localhost:3000/load_data')).text();
+    // let roomsAmount = await (await fetch('http://localhost:3000/load_data')).text();
+    let roomsAmount = 4
     return { 
         roomsAmount: roomsAmount 
     };
